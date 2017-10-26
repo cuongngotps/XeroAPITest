e7f616a6d04b9f889771a7e65c4f8271406d9702
cuongngotps <cuong.ngo@tpssoft.com>
2017-10-26 15:26:44 +0700

    integrate library simple

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
 using Xero.Api.Core;
-using Xero.Api.Example.Applications.Partner;
 using Xero.Api.Example.Applications.Private;
-using Xero.Api.Example.Applications.Public;
-using Xero.Api.Example.TokenStores;
 using Xero.Api.Infrastructure.OAuth;
 using Xero.Api.Serialization;
 
@@ -18,30 +13,43 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             // Private Application Sample
-            var private_app_api = new XeroCoreApi("https://api.xero.com", new PrivateAuthenticator(@"C:\Dev\your_public_privatekey.pfx"),
-                new Consumer("your-consumer-key", "your-consumer-secret"), null,
+
+            X509Certificate2 cert = new X509Certificate2();
+            cert.Import(@"..\public_privatekey.pfx", "NMCuong@1992", X509KeyStorageFlags.MachineKeySet);
+
+            var private_app_api = new XeroCoreApi("https://api.xero.com/api.xro/2.0/", new PrivateAuthenticator(cert),
+                new Consumer("WQLCRSSUUFJFEFHQ7KHKPV43GFJC7F", "ML6MSKXJOYBH5ZBFYHZXH5Z3WGTGPM"), null,
                 new DefaultMapper(), new DefaultMapper());
 
             var org = private_app_api.Organisation;
 
+            var invoices = private_app_api.Invoices.OrderByDescending("Total").Find();
+            Console.WriteLine(invoices.Count());
+
+            foreach (var invoice in invoices)
+            {
+                Console.WriteLine(invoice.Contact.Name + ", " + invoice.Contact.Name);
+            }
+
+
             var user = new ApiUser { Name = Environment.MachineName };
 
             // Public Application Sample
-            var public_app_api = new XeroCoreApi("https://api.xero.com", new PublicAuthenticator("https://api.xero.com", "https://api.xero.com", "oob",
-                new MemoryTokenStore()),
-                new Consumer("your-consumer-key", "your-consumer-secret"), user,
-                new DefaultMapper(), new DefaultMapper());
+            //var public_app_api = new XeroCoreApi("https://api.xero.com", new PublicAuthenticator("https://api.xero.com", "https://api.xero.com", "oob",
+            //    new MemoryTokenStore()),
+            //    new Consumer("your-consumer-key", "your-consumer-secret"), user,
+            //    new DefaultMapper(), new DefaultMapper());
 
-            var public_contacts = public_app_api.Contacts.Find().ToList();
+            //var public_contacts = public_app_api.Contacts.Find().ToList();
 
             // Partner Application Sample
-            var partner_app_api = new XeroCoreApi("https://api-partner.network.xero.com", new PartnerAuthenticator("https://api-partner.network.xero.com",
-                "https://api.xero.com", "oob", new MemoryTokenStore(),
-                @"C:\Dev\your_public_privatekey.pfx"),
-                 new Consumer("your-consumer-key", "your-consumer-secret"), user,
-                 new DefaultMapper(), new DefaultMapper());
+            //var partner_app_api = new XeroCoreApi("https://api-partner.network.xero.com", new PartnerAuthenticator("https://api-partner.network.xero.com",
+            //    "https://api.xero.com", "oob", new MemoryTokenStore(),
+            //    @"C:\Dev\your_public_privatekey.pfx"),
+            //     new Consumer("your-consumer-key", "your-consumer-secret"), user,
+            //     new DefaultMapper(), new DefaultMapper());
 
-            var partner_contacts = partner_app_api.Contacts.Find().ToList();
+            //var partner_contacts = partner_app_api.Contacts.Find().ToList();
         }
     }
 }
